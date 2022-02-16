732153c13d61d2a3fba02a172953130b0da5a018
Víctor Barreto <victorbarretolins@gmail.com>
2022-02-16 11:17:53 -0300

    finished the post route

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ main()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname + '/views'))
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (request, response) => {
     response.render('home.ejs')
 })
@@ -35,12 +38,22 @@ app.get('/campgrounds/new', async (request, response) => {
     response.render('campgrounds/new')
 })
 
+app.post('/campgrounds', async (request, response) => {
+    const campground = await Campground(request.body.campground)
+    await campground.save()
+    console.log(campground)
+    response.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.get('/campgrounds/:id', async (request, response) => {
     const campground = await Campground.findById(request.params.id)
     response.render('campgrounds/show', {campground})
 })
 
+app.get('/campgrounds/:id/edit', async (request, response) => {
+    const campground = await Campground.findById(request.params.id)
+    response.render('campgrounds/edit', {campground})
+})
 
 app.listen(8000, () => {
     console.log('Listening on port 8000')
