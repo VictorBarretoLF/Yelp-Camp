c028715e36c4592e83382f407e72beff22f2e0b0
Víctor Barreto <victorbarretolins@gmail.com>
2022-02-16 15:11:14 -0300

    finished the delete router

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const { Campground } = require('./models/campground')
+const methodOverride = require('method-override')
+const { response } = require('express')
 
 async function main(){
     try {
@@ -20,20 +22,19 @@ app.set('views', path.join(__dirname + '/views'))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/', (request, response) => {
     response.render('home.ejs')
 })
 
-app.get('/make', async (request, response) => {
-    response.render('home')
-})
-
+// SEE THE LIST OF CAMPGROUNDS
 app.get('/campgrounds', async (request, response) => {
     const campgrounds = await Campground.find({})
     response.render('campgrounds/index', {campgrounds})
 })
 
+// CREATE NEW CAMPGROUNDS
 app.get('/campgrounds/new', async (request, response) => {
     response.render('campgrounds/new')
 })
@@ -45,15 +46,33 @@ app.post('/campgrounds', async (request, response) => {
     response.redirect(`/campgrounds/${campground._id}`)
 })
 
+// SEE JUST ONE CAMPGROUND INFORMATIONS
 app.get('/campgrounds/:id', async (request, response) => {
     const campground = await Campground.findById(request.params.id)
     response.render('campgrounds/show', {campground})
 })
 
+// EDIT
 app.get('/campgrounds/:id/edit', async (request, response) => {
     const campground = await Campground.findById(request.params.id)
     response.render('campgrounds/edit', {campground})
 })
+
+app.patch('/campgrounds/:id', async (request, response) => {
+    await Campground.updateOne({_id : request.params.id}, {$set : request.body.campground})
+    console.log(request.params.id)
+    response.redirect(`/campgrounds/${request.params.id}`)
+})
+
+// DELETE ROUTE
+app.delete('/campgrounds/:id', async (request, response) => {
+    await Campground.findByIdAndDelete(request.params.id)
+    response.redirect('/campgrounds')
+})
+
+
+
+
 
 app.listen(8000, () => {
     console.log('Listening on port 8000')
