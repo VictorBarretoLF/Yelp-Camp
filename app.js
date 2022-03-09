b42c5b265507b63395924785edc0679f26d88c53
Víctor Barreto <victorbarretolins@gmail.com>
2022-03-09 15:44:08 -0300

    add boilerplate.ejs layout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const { Campground } = require('./models/campground')
 const methodOverride = require('method-override')
 
@@ -16,6 +17,7 @@ async function main(){
 }
 main()
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname + '/views'))
 
