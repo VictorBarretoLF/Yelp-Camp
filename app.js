dc8720f70139c74c09f451c2e0eead49f211b347
Víctor Barreto <victorbarretolins@gmail.com>
2022-03-09 16:05:58 -0300

    got rid of the styles using pure css

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname + '/views'))
 
-app.use(express.static(path.join(__dirname + '/styles')))
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
